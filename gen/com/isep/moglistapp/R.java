3c58e8397dfd796e5f0696c09fd9edf762534769
dorianeselva <doriane.selva@gmail.com>
2015-01-20 14:58:02 +0100

    design

--- gen/com/isep/moglistapp/R.java
+++ gen/com/isep/moglistapp/R.java
@@ -29,13 +29,13 @@ public final class R {
         public static final int ic_action_new=0x7f020004;
         public static final int ic_action_refresh=0x7f020005;
         public static final int ic_launcher=0x7f020006;
-        public static final int ic_stat_name=0x7f020007;
-        public static final int settings=0x7f020008;
+        public static final int settings=0x7f020007;
     }
     public static final class id {
-        public static final int action_new=0x7f08000e;
-        public static final int action_refresh=0x7f08000d;
-        public static final int action_settings=0x7f08000c;
+        public static final int action_new=0x7f08000f;
+        public static final int action_refresh=0x7f08000e;
+        public static final int action_settings=0x7f08000d;
+        public static final int bell=0x7f08000c;
         public static final int btnNewList=0x7f080008;
         public static final int check_password=0x7f080009;
         public static final int connection=0x7f08000b;
@@ -54,10 +54,9 @@ public final class R {
         public static final int activity_home=0x7f030001;
         public static final int activity_main=0x7f030002;
         public static final int activity_my_lists=0x7f030003;
-        public static final int activity_new_list=0x7f030004;
-        public static final int activity_newlist=0x7f030005;
-        public static final int activity_registration=0x7f030006;
-        public static final int list_item_layout=0x7f030007;
+        public static final int activity_newlist=0x7f030004;
+        public static final int activity_registration=0x7f030005;
+        public static final int list_item_layout=0x7f030006;
     }
     public static final class menu {
         public static final int main=0x7f070000;
@@ -69,6 +68,7 @@ public final class R {
         public static final int action_refresh=0x7f05000b;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int bell=0x7f05000c;
         public static final int forgot_password=0x7f050003;
         public static final int hello_world=0x7f050007;
         public static final int login=0x7f050002;
